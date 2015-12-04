5e75a349e417e3c76ea12a9eb043ee62d8beeaa0
sridharaiyer <iyer.a.sri@gmail.com>
2015-12-03 23:59:16 -0600

    My first bootstrap landing  page

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    $('section#screenshots a').on('click', function() {
+        $('div#modal img').attr('src', $(this).attr('data-img-url'));
+    });
+    var nav = $('.navbar-fixed-top');
+    var distance = $('.navbar-fixed-top').offset();
+
+    if (distance.top >= 300) {
+        nav.addClass('effect');
+    }
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 300) {
+            nav.addClass('effect');
+        } else {
+            nav.removeClass('effect');
+        }
+    });
+
+    $('#about .blue-circle').waypoint(function() {
+        $('#about .blue-circle').addClass('animated fadeInUp')
+    }, {
+        offset: '50%'
+    });
+    $('#features .blue-circle').waypoint(function() {
+        $(this.element).addClass('animated fadeInUp')
+    }, {
+        offset: '50%'
+    });
+    $('#screenshots .col-sm-4').waypoint(function() {
+        $(this.element).addClass('animated zoomIn');
+        $(this.element).css({
+            'opacity': 1
+        })
+    }, {
+        offset: '50%'
+    });
+
+    $('.features-image img').waypoint(function() {
+        $('.features-image img').addClass('animated pulse')
+    }, {
+        offset: '50%'
+    });
+    $('#download div.phone img').waypoint(function() {
+        $(this.element).addClass('animated fadeInRight')
+    }, {
+        offset: '50%'
+    });    $('#download section div').waypoint(function() {
+        $(this.element).addClass('animated fadeInDown')
+    }, {
+        offset: '50%'
+    });
+});
+
+smoothScroll.init({
+    speed: 700,
+    easing: 'easeInOutQuad',
+    updateURL: false
+});
